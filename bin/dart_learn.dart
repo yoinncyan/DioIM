62e574cac5fb4b956dd287a268d72b4151e48af6
yoinncyan <yoinncyan2306@icloud.com>
2025-12-06 03:00:41 +0900

    commit0

---
+++ bin/dart_learn.dart
@@ -0,0 +1,12 @@
+// import 'package:dart_learn/dart_learn.dart' as dart_learn;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${dart_learn.calculate()}!');
+// }
+
+void main(List<String> args) {
+
+dynamic a = 'Hello Dart';
+
+  print(a);
+}
